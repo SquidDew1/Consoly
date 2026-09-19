f4998313359b8469c003dc58a58ce05d5c5f8b03
SquidDew1 <squiddew1@gmail.com>
2026-09-19 10:26:13 +0200

    Removed useless things and initialized window

--- common/src/main/java/me/squiddew/consoly/ConsolyMod.java
+++ common/src/main/java/me/squiddew/consoly/ConsolyMod.java
@@ -1,17 +1,16 @@
 package me.squiddew.consoly;
 
-import me.squiddew.consoly.platform.Services;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-
 public class ConsolyMod {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger("Consoly");
     public static final String MOD_ID = "consoly";
 
-    public static void init() {
-        if (Services.PLATFORM.isModLoaded(MOD_ID)){
-            LOGGER.info("Consoly loaded on {}", Services.PLATFORM.getPlatformName());
-        }
+    public static void onInitializeCommon() {
+        Window.create(
+                "Consoly",
+                800,
+                700,
+                true,
+                "/assets/consoly/icon.png"
+        );
     }
 }
