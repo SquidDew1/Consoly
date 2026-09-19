66e4b28c29e8054a1139363b44fe029cc2aa7eaa
SquidDew1 <squiddew1@gmail.com>
2026-09-19 14:56:38 +0200

    Added a method to close the window

--- common/src/main/java/me/squiddew/consoly/Window.java
+++ common/src/main/java/me/squiddew/consoly/Window.java
@@ -1,15 +1,21 @@
 package me.squiddew.consoly;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.net.URL;
 
 public class Window {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger("Consoly");
+    private static JFrame frame;
+
     public static void create(String title, int width, int height, boolean icon, String iconPath){
         System.setProperty("java.awt.headless", "false");
 
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame(title);
+            frame = new JFrame(title);
             frame.setSize(width, height);
             frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -21,6 +27,16 @@ public class Window {
             }
             frame.setResizable(true);
             frame.setVisible(true);
+        });
+    }
+    public static void close(){
+        SwingUtilities.invokeLater(() -> {
+            if (frame != null){
+                LOGGER.info("Stopping!");
+                frame.setVisible(false);
+                frame.dispose();
+                frame = null;
+            }
         });
     }
 }
