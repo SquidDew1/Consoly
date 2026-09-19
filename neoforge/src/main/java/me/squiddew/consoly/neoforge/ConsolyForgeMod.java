efb52ae02fcb906c30a228622b1857b421383aaa
SquidDew1 <squiddew1@gmail.com>
2026-09-19 22:20:43 +0200

    Added NeoForge config for Consoly

--- neoforge/src/main/java/me/squiddew/consoly/neoforge/ConsolyForgeMod.java
+++ neoforge/src/main/java/me/squiddew/consoly/neoforge/ConsolyForgeMod.java
@@ -1,13 +1,21 @@
 package me.squiddew.consoly.neoforge;
 
 import me.squiddew.consoly.ConsolyMod;
+import me.squiddew.consoly.screen.ConsolyScreen;
+import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod(ConsolyMod.MOD_ID)
+@Mod(value = ConsolyMod.MOD_ID, dist = Dist.CLIENT)
 public class ConsolyForgeMod {
 
-    public ConsolyForgeMod() {
+    public ConsolyForgeMod(ModContainer modContainer) {
 
+        modContainer.registerExtensionPoint(IConfigScreenFactory.class,
+                (_, _) -> new ConsolyScreen(Component.literal("Consoly"))
+        );
         ConsolyMod.onInitializeCommon();
     }
 }
