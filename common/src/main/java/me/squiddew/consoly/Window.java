c16992aeb5ecece1e43f43be85412fa981aa3349
SquidDew1 <squiddew1@gmail.com>
2026-09-21 19:56:31 +0200

    Write the console down

--- common/src/main/java/me/squiddew/consoly/Window.java
+++ common/src/main/java/me/squiddew/consoly/Window.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
+import java.awt.*;
 import java.net.URL;
 
 public class Window {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("Consoly");
     private static JFrame frame;
+    public static JTextArea textArea;
 
     public static void create(String title, int width, int height, boolean icon, String iconPath){
         System.setProperty("java.awt.headless", "false");
@@ -28,6 +30,18 @@ public class Window {
             frame.setResizable(true);
             frame.setVisible(true);
         });
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+
+        scrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(5, 0));
+        scrollPane.getVerticalScrollBar().setBackground(new Color(0, 0, 0));
+
+        scrollPane.getHorizontalScrollBar().setPreferredSize(new Dimension(0, 5));
+        scrollPane.getHorizontalScrollBar().setBackground(new Color(0, 0, 0));
+
+        frame.add(scrollPane, BorderLayout.CENTER);
     }
     public static void close(){
         SwingUtilities.invokeLater(() -> {
