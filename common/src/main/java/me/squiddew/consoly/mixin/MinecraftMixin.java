b644ec856937fd1562846cab0ca53f0e287618f6
SquidDew1 <squiddew1@gmail.com>
2026-09-25 20:52:24 +0200

    Use a Mixin instead of onInitializeCommon() to avoid issues

--- common/src/main/java/me/squiddew/consoly/mixin/MinecraftMixin.java
+++ common/src/main/java/me/squiddew/consoly/mixin/MinecraftMixin.java
@@ -1,7 +1,9 @@
 package me.squiddew.consoly.mixin;
 
-import me.squiddew.consoly.Window;
+import me.squiddew.consoly.ConsoleWindow;
+import me.squiddew.consoly.ConsolyOptions;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.main.GameConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,6 +14,17 @@ public class MinecraftMixin {
 
     @Inject(method = "stop", at = @At("HEAD"))
     private void consoly$close(CallbackInfo ci){
-        Window.close();
+        ConsoleWindow.close();
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void consoly$createWindow(GameConfig gameConfig, CallbackInfo ci){
+        ConsoleWindow.createWindow(
+                "Console",
+                800,
+                700,
+                true,
+                "/assets/consoly/icon.png",
+                ConsolyOptions.dark ? 64 : 225);
     }
 }
